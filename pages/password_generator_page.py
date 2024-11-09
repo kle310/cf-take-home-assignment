18559fea14d8fbae27a7de0692317d2be49ea90c
kle310 <khacle@gmail.com>
2024-11-09 12:49:15 -0800

    Initial commit of completed assignment

---
+++ pages/password_generator_page.py
@@ -0,0 +1,45 @@
+from playwright.sync_api import Page
+
+class PasswordGeneratorPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.password_length_input = page.locator("#passwordLength")
+        self.lowercase_checkbox = page.locator("#option-lowercase")
+        self.lowercase_label = page.locator("label[for='option-lowercase']")
+        self.uppercase_checkbox = page.locator("#option-uppercase")
+        self.uppercase_label = page.locator("label[for='option-uppercase']")
+        self.numbers_checkbox = page.locator("#option-numbers")
+        self.numbers_label = page.locator("label[for='option-numbers']")
+        self.symbols_checkbox = page.locator("#option-symbols")
+        self.symbols_label = page.locator("label[for='option-symbols']")
+        self.generate_button = page.get_by_title("Generate password")
+        # self.copy_button = page.get_by_title("Copy Password")
+        self.copy_button = page.locator('button[title="Copy Password"]')
+        self.password_output = page.locator("#password")
+        self.copy_password_button = page.get_by_title("Copy Password")
+        self.symbols_set = symbols = set(r"! # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~")
+
+    def set_password_length(self, length: int):
+        self.password_length_input.fill(str(length))
+
+    def get_generated_password(self) -> str:
+        return self.password_output.input_value()
+
+    def get_clipboard_content(self) -> str:
+        return self.page.evaluate("navigator.clipboard.readText()")
+
+    def toggle_uppercase(self, enable: bool):
+        if self.uppercase_checkbox.is_checked() != enable:
+            self.uppercase_label.click()
+
+    def toggle_lowercase(self, enable: bool):
+        if self.lowercase_checkbox.is_checked() != enable:
+            self.lowercase_label.click()
+
+    def toggle_numbers(self, enable: bool):
+        if self.numbers_checkbox.is_checked() != enable:
+            self.numbers_label.click()
+            
+    def toggle_symbols(self, enable: bool):
+        if self.symbols_checkbox.is_checked() != enable:
+            self.symbols_label.click()
---
+++ tests/test_password_generator.py
@@ -0,0 +1,79 @@
+import pytest
+from pages.password_generator_page import PasswordGeneratorPage
+from playwright.sync_api import Page
+
+@pytest.fixture
+def password_generator_page(page: Page):
+    page.goto("https://www.security.org/password-generator/")
+    return PasswordGeneratorPage(page)
+
+def test_generate_password_with_recommended_settings(password_generator_page):
+    password_generator_page.set_password_length(12)
+    password_generator_page.toggle_lowercase(True)
+    password_generator_page.toggle_uppercase(True)
+    password_generator_page.toggle_numbers(True)
+    password_generator_page.toggle_symbols(True)
+    password = password_generator_page.get_generated_password()
+    assert len(password) == 12, "Password length should be 12."
+    assert any(char.isupper() for char in password), "Password should contain uppercase letters."
+    assert any(char.islower() for char in password), "Password should contain lowercase letters."
+    assert any(char.isdigit() for char in password), "Password should contain numbers."
+    assert any(char in password_generator_page.symbols_set for char in password), "Password should contain symbols."
+
+def test_able_to_toggle_options(password_generator_page):
+    #the default state is True, True, False, False
+    password_generator_page.toggle_lowercase(True)
+    password_generator_page.toggle_uppercase(True)
+    password_generator_page.toggle_numbers(True)
+    password_generator_page.toggle_symbols(True)
+
+    password_generator_page.toggle_lowercase(False)
+    password = password_generator_page.get_generated_password()
+    assert not any(char.islower() for char in password), "Unable to generate password without lowercase."
+
+    password_generator_page.toggle_uppercase(False)
+    password = password_generator_page.get_generated_password()
+    assert not any(char.isupper() for char in password), "Unable to generate password without uppercase."
+
+    password_generator_page.toggle_numbers(False)
+    password = password_generator_page.get_generated_password()
+    assert not any(char.isdigit() for char in password), "Unable to generate password without numbers."
+
+    #need to enable at least one before turning off symbols
+    password_generator_page.toggle_lowercase(True)
+    password_generator_page.toggle_symbols(False)
+    password = password_generator_page.get_generated_password()
+    assert not any(char in password_generator_page.symbols_set for char in password), "Unable to generate password without symbols."
+
+def test_password_regenerate(password_generator_page):
+    old_password = password_generator_page.get_generated_password()
+    password_generator_page.generate_button.click()
+    new_password = password_generator_page.get_generated_password()
+    assert old_password != new_password, "Unable to regenerate password"
+
+def test_generate_password_with_custom_length(password_generator_page):
+    password_generator_page.set_password_length(12)
+    password_generator_page.generate_button.click()
+    password = password_generator_page.get_generated_password()
+    assert len(password) == 12, "Unable to set password length between 6-32"
+
+    password_generator_page.set_password_length(1)
+    password_generator_page.generate_button.click()
+    password = password_generator_page.get_generated_password()
+    assert len(password) == 6, "User was able to set password length to less than min of 6"
+
+    password_generator_page.set_password_length(33)
+    password_generator_page.generate_button.click()
+    password = password_generator_page.get_generated_password()
+    assert len(password) == 32, "User was able to set password length to more than max of 32"
+
+def test_copy_password_to_clipboard(password_generator_page):
+    # Grant necessary clipboard permissions
+    context = password_generator_page.page.context
+    context.grant_permissions(["clipboard-read"])  
+
+    password = password_generator_page.get_generated_password()
+    password_generator_page.copy_button.click()
+    clipboard = password_generator_page.get_clipboard_content()
+
+    assert password == clipboard, "Copy to clipboard does not work"
